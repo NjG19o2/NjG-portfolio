29ba8404e07debd34ce81393dd076aa2a0148c74
NjG19o2 <njg19o2@gmail.com>
2024-07-03 22:31:29 +0530

    initialize component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Project from './Components/Project';
+import Skills from './Components/Skills';
+import Contact from './Components/Contact';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>NjG</p>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      <Route exact path='/home' element={<Home/>}/>
+      <Route exact path='/about' element={<About/>}/>
+      <Route exact path='/skills' element={<Skills/>}/>
+      <Route exact path='/project' element={<Project/>}/>
+      <Route exact path='/contact' element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">NjG</Link>
+    <buttonLink className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </buttonLink>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/skills">Skills</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/project">Project</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
