c675d16566b466cb00bd8159dcb0791e13e7ef98
NjG19o2 <njg19o2@gmail.com>
2024-07-09 02:11:14 +0530

    Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">NjG</Link>
-    <buttonLink className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className="navbar navbar-expand-lg bg-dark color-white ">
+  <div className="container-fluid" style={{margin: "0px 103px"}}>
+    <NavLink className="navbar-brand text-white" to="/">NjG</NavLink>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
-    </buttonLink>
+    </button>
     <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item" style={{ margin: "0 15px" }}>
+          <NavLink className="nav-link" to="/home" activeClassName="active-link">Home</NavLink>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/about">About</Link>
+        <li className="nav-item" style={{ margin: "0 15px" }}>
+          <NavLink className="nav-link" to="/about" activeClassName="active-link">About</NavLink>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/skills">Skills</Link>
+        <li className="nav-item" style={{ margin: "0 15px" }}>
+          <NavLink className="nav-link" to="/skills" activeClassName="active-link">Skills</NavLink>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/project">Project</Link>
+        <li className="nav-item" style={{ margin: "0 15px" }}>
+          <NavLink className="nav-link" to="/project" activeClassName="active-link">Project</NavLink>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/contact">Contact</Link>
+        <li className="nav-item" style={{ margin: "0 15px" }}>
+          <NavLink className="nav-link" to="/contact" activeClassName="active-link">Contact</NavLink>
         </li>
       </ul>
     </div>
